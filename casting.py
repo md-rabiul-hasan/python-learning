7a7fe2cc7f2f315e0fb0e5602650547b3cd3cde5
Md.Rabiul Hasan <70057377+md-rabiul-hasan@users.noreply.github.com>
2023-06-01 12:30:29 +0600

    type-casting

---
+++ casting.py
@@ -0,0 +1,18 @@
+# integer type casting
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+
+
+
+# float type casting
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
+
+
+# string type casting
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
